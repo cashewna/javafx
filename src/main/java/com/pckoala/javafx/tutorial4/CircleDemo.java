c328217f3d9193b8a7d0a629e4c2618948a32a34
banana <krishnakhong@gmail.com>
2022-09-22 23:02:00 +0800

    Added CircleDemo.java and initialized GreenForGo.java

--- src/main/java/com/pckoala/javafx/tutorial4/CircleDemo.java
+++ src/main/java/com/pckoala/javafx/tutorial4/CircleDemo.java
@@ -2,9 +2,11 @@ package com.pckoala.javafx.tutorial4;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class CircleDemo extends Application {
@@ -13,13 +15,35 @@ public class CircleDemo extends Application {
   public void start(Stage primaryStage) {
     // Create a pane and the green and red circles
     Pane pane = new Pane();
-    Circle circleRed = new Circle(100, 90, 5);
-    Circle circleGreen = new Circle(240, 239, 5);
+    Circle circleRed = new Circle(100, 90, 15);
+    Circle circleGreen = new Circle(240, 239, 15);
     circleRed.setFill(Color.RED);
     circleGreen.setFill(Color.GREEN);
 
-    // Add the circles to the pane
-    pane.getChildren().addAll(circleRed, circleGreen);
+    // Create a line connecting the center of the two circles
+    Line line =
+        new Line(
+            circleRed.getCenterX(),
+            circleRed.getCenterY(),
+            circleGreen.getCenterX(),
+            circleGreen.getCenterY());
+
+
+    // Add a label calculating the length of the line
+    // hypotenuse = sqrt(a^2 + b^2)
+    double startX = line.getStartX();
+    double endX = line.getEndX();
+    double startY = line.getStartY();
+    double endY = line.getEndY();
+    Label label = new Label(
+        Double.toString(Math.sqrt(Math.pow((endX - startX), 2) + Math.pow((endY - startY), 2)))
+    );
+
+    label.setLayoutX(endX - startX + 20);
+    label.setLayoutY(endY - startY - 10);
+
+    // Add the circles and the line to the pane
+    pane.getChildren().addAll(circleRed, circleGreen, line, label);
 
     // Create a scene and place it in the stage
     Scene scene = new Scene(pane, 400, 400);
---
+++ src/main/java/com/pckoala/javafx/tutorial4/GreenForGo.java
@@ -0,0 +1,12 @@
+package com.pckoala.javafx.tutorial4;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GreenForGo extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+
+  }
+}
