ceb68d72fa2af047a4fc0d65026c257bde242b84
banana <krishnakhong@gmail.com>
2022-09-22 14:54:28 +0800

    Added Loan class and LoanCalculator.java

---
+++ src/main/java/com/pckoala/javafx/chapter10/Loan.java
@@ -0,0 +1,60 @@
+package com.pckoala.javafx.chapter10;
+
+import java.util.Date;
+
+public class Loan {
+  private double annualInterestRate;
+  private int numberOfYears;
+  private double loanAmount;
+  private final Date loanDate;
+
+  public Loan() {
+    this(2.5, 1, 1000);
+  }
+
+  public Loan(double annualInterestRate, int numberOfYears, double loanAmount) {
+    this.annualInterestRate = annualInterestRate;
+    this.numberOfYears = numberOfYears;
+    this.loanAmount = loanAmount;
+    loanDate = new Date();
+  }
+
+  public double getAnnualInterestRate() {
+    return annualInterestRate;
+  }
+
+  public void setAnnualInterestRate(double annualInterestRate) {
+    this.annualInterestRate = annualInterestRate;
+  }
+
+  public int getNumberOfYears() {
+    return numberOfYears;
+  }
+
+  public void setNumberOfYears(int numberOfYears) {
+    this.numberOfYears = numberOfYears;
+  }
+
+  public double getLoanAmount() {
+    return loanAmount;
+  }
+
+  public void setLoanAmount(double loanAmount) {
+    this.loanAmount = loanAmount;
+  }
+
+  public double getMonthlyPayment() {
+    double monthlyInterestRate = annualInterestRate / 12;
+    return loanAmount
+        * monthlyInterestRate
+        / (1 - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
+  }
+
+  public double getTotalPayment() {
+    return getMonthlyPayment() * numberOfYears * 12;
+  }
+
+  public Date getLoanDate() {
+    return loanDate;
+  }
+}
---
+++ src/main/java/com/pckoala/javafx/chapter15/LoanCalculator.java
@@ -0,0 +1,62 @@
+package com.pckoala.javafx.chapter15;
+import com.pckoala.javafx.chapter10.Loan;
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class LoanCalculator extends Application {
+  private TextField tfAnnualInterestRate = new TextField();
+  private TextField tfNumberOfYears = new TextField();
+  private TextField tfLoanAmount = new TextField();
+  private TextField tfMonthlyPayment = new TextField();
+  private TextField tfTotalPayment = new TextField();
+  private Button btCalculate = new Button();
+
+  @Override
+  public void start(Stage primaryStage) {
+    // Create UI
+    GridPane gridPane = new GridPane();
+    gridPane.setHgap(5);
+    gridPane.setVgap(5);
+    gridPane.add(new Label("Annual Interest Rate:"), 0, 0);
+    gridPane.add(tfAnnualInterestRate, 1, 0);
+    gridPane.add(new Label("Number of Years:"), 0, 1);
+    gridPane.add(tfNumberOfYears, 1, 1);
+    gridPane.add(new Label("Loan Amount:"), 0, 2);
+    gridPane.add(tfLoanAmount, 1, 2);
+    gridPane.add(new Label("Monthly Payment:"), 0, 3);
+    gridPane.add(tfMonthlyPayment, 1, 3);
+    gridPane.add(new Label("Total Payment:"), 0, 4);
+    gridPane.add(tfTotalPayment, 1, 4);
+    gridPane.add(btCalculate, 1, 5);
+
+    // Set properties for UI
+    gridPane.setAlignment(Pos.CENTER);
+    tfAnnualInterestRate.setAlignment(Pos.BOTTOM_RIGHT);
+    tfNumberOfYears.setAlignment(Pos.BOTTOM_RIGHT);
+    tfLoanAmount.setAlignment(Pos.BOTTOM_RIGHT);
+    tfMonthlyPayment.setAlignment(Pos.BOTTOM_RIGHT);
+    tfTotalPayment.setAlignment(Pos.BOTTOM_RIGHT);
+    tfMonthlyPayment.setEditable(false);
+    tfTotalPayment.setEditable(false);
+    GridPane.setHalignment(btCalculate, HPos.RIGHT);
+
+    // Process events
+    btCalculate.setOnAction(event -> calculateLoanPayment());
+  }
+
+  private void calculateLoanPayment() {
+    // Get values from text fields
+    double interest = Double.parseDouble(tfAnnualInterestRate.getText());
+    int years = Integer.parseInt(tfNumberOfYears.getText());
+    double loanAmount = Double.parseDouble(tfLoanAmount.getText());
+
+    // Create a loan object
+    Loan loan = new Loan(interest, years, loanAmount);
+  }
+}
